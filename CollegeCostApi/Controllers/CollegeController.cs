30a0f382aeec1c5a44381801c8d9f8891f458663
Joshua Stoltenberg <stoltenberg96@gmail.com>
2019-01-07 22:18:59 -0600

    Request with room and board request added

--- CollegeCostApi/Controllers/CollegeController.cs
+++ CollegeCostApi/Controllers/CollegeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using CollegeCostApi.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.IO;
+using System.Net;
+using static System.Net.WebRequestMethods;
 
 namespace CollegeCostApi.Controllers
 {
@@ -20,18 +18,15 @@ namespace CollegeCostApi.Controllers
             colleges = CollegeData.GetCollegesFromCSV("college_costs.csv");
         }         
 
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<College>> Get()
-        {            
-            return colleges;
-        }
-
         // GET api/values/(name)
         [HttpGet("{name}")]
-        public ActionResult<double> Get(string name)
+        public ActionResult<double> GetTotalCostByName(string name)
         {
             College selectedCollege = new College();
+            if (string.IsNullOrEmpty(name) || name == "true" || name == "false")
+            {
+                return NotFound("College Name Required");
+            }
             foreach (College c in colleges)
             {
                 if (c.Name == name)
@@ -39,13 +34,37 @@ namespace CollegeCostApi.Controllers
                     selectedCollege = c;
                 }
             }
-            if (selectedCollege == null)
+            if (selectedCollege.Name == null)
             {
-                return NotFound();
+                return NotFound("Error: College not Found");
             }
             double tuitionCost = selectedCollege.InStateTution + selectedCollege.RoomBoardCost;
             return tuitionCost;
         }
-        
+
+        [HttpGet("{name}/{includeRoomAndBoard}")]
+        public ActionResult<double> GetCostWithRoomAndBoard(string name, string includeRoomAndBoard)
+        {
+            bool addRoomAndBoard = includeRoomAndBoard == "false" ? false : true;
+            College selectedCollege = new College();
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new HttpListenerException(400, "College Name Required");
+            }
+            foreach (College c in colleges)
+            {
+                if (c.Name == name)
+                {
+                    selectedCollege = c;
+                }
+            }
+            if (selectedCollege.Name == null)
+            {
+                return NotFound("Error: College not Found");
+            }
+            double tuitionCost = addRoomAndBoard ? selectedCollege.InStateTution + selectedCollege.RoomBoardCost : selectedCollege.InStateTution;
+            return tuitionCost;
+        }
+
     }
 }
